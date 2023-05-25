7343ca238238f9764db269f1ab41f18887430e51
Dan Ruan <danrsaquetti@gmail.com>
2023-05-24 22:30:59 -0300

    update schema

--- prisma/migrations/20230523235437_add_schema_test/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "test" (
-    "id" SERIAL NOT NULL,
-    "text" TEXT NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
-);
